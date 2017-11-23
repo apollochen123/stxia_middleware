431a244345d8907112a4c7a33fc36f50f36dea55
apollochen123 <apollo.chan@qq.com>
2017-11-23 22:36:14 +0800

    how to post

--- src/main/java/com/stxia/middleware/MiddlewareMain.java
+++ src/main/java/com/stxia/middleware/MiddlewareMain.java
@@ -14,15 +14,27 @@
  */
 package com.stxia.middleware;
 
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.apache.log4j.Logger;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
+import com.alibaba.fastjson.JSONObject;
 import com.stxia.middleware.configuration.MyMappingJackson2HttpMessageConverter;
 import com.stxia.middleware.upload.UploadData;
 
@@ -48,15 +60,13 @@ public class MiddlewareMain
 //        WaterMeterSwitch wms = new WaterMeterSwitch();
 //        wms.swi();
         RestTemplate restTemplate  = new RestTemplate();
-        Map<String,String> map = new HashMap<>();
-        map.put("method", "hwa.hvalve.close");
-        map.put("meter_sn", "52000002");
-        map.put("app_sn", "7573f4927e5ee92e");
-        map.put("extend", "hwa");
-        map.put("user_token", "9cab82811914830847007c9cf30670a2");
-        
-        String a  = restTemplate.postForObject("http://47.93.227.123", null, String.class, map);
-        System.out.println(a);
+        String bodyValTemplate = "method=hwa.hvalve.close&meter_sn=52000002&app_sn=&7573f4927e5ee92e&extend=hwa&user_token=9cab82811914830847007c9cf30670a2";
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+        HttpEntity<String> entity = new HttpEntity<String>(bodyValTemplate, headers);
+        ResponseEntity<String> a = restTemplate.exchange("http://47.93.227.123/", HttpMethod.POST, entity, String.class);
+        JSONObject j = JSONObject.parseObject(a.getBody());
+        System.out.println(j.get("data_value"));
     }
     @Bean
     public RestTemplate getRestTemplate(){
